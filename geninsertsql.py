0e6225c54abfb2d550479e5b84a639908b205f83
Leotis <leotisbuchanan@gmail.com>
2017-03-17 22:55:21 -0400

    add semicolon

--- geninsertsql.py
+++ geninsertsql.py
@@ -4,12 +4,12 @@ rows = len(df)
 for idx in range(0, rows):
     date = df.ix[idx]['dt']
     country  = df.ix[idx]['Country']
-    averagetemperature = df.ix[idx]['AverageTemperature']
+    averagetemperature = df.ix[idx]['AverageTemperat"ure']
     city = df.ix[idx]['City']
     latitude = df.ix[idx]['Latitude']
     longitude = df.ix[idx]['Longitude']
     averagetemperatureuncertainty = df.ix[idx]['AverageTemperatureUncertainty']
     query ="""INSERT INTO GlobalLandTemperaturesByCity_10000 (dt, Country, AverageTemperature,AverageTemperatureUncertainty, Longitude, Latitude,City)
-    VALUES ("{dt}","{country}",{averagetemperature},{longitude},{latitude},"{city}")""".format(dt=date, country=country,averagetemperature=averagetemperature, longitude=longitude, latitude=latitude, city=city) 
+    VALUES ("{dt}","{country}",{averagetemperature},"{longitude}","{latitude}","{city}");""".format(dt=date, country=country,averagetemperature=averagetemperature, longitude=longitude, latitude=latitude, city=city) 
     print(query)
 
